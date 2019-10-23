b926ff15a36bab8f52e56a9bf99411279d4237c7
Pankraz01 <mine6626@gmail.com>
2019-10-23 10:06:40 +0200

    added JavaDoc to TextUmkehren

--- src/dhbw/daniel/main/PiBerechnen.java
+++ src/dhbw/daniel/main/PiBerechnen.java
@@ -5,43 +5,20 @@ package dhbw.daniel.main;
  * Zurzeit WIP
  */
 public class PiBerechnen {
-    static int tropfenzahl = 1453739826; // Je höher desto Langsamer, aber genauere, Annhäherung an Pi
 
+    static double pi;
 
-    /**
-     *
-     * @param args
-     */
     public static void piBerechnen(String[] args) {
         PiBerechnen piBerechnen = new PiBerechnen();
     }
 
-    /**
-     * Sehr Langsame Annäherung von Pi
-     */
     public static void calcPi() {
-
-        double pi = 0;
-        int innerhalb = 0;
-        int gesamt = tropfenzahl;
-
-        while (tropfenzahl > 0) { // generiere Tropfen und addiere je nach Zugehörigkeit
-            double dotx = 2 * Math.random() - 1;
-            double doty = 2 * Math.random() - 1;
-
-            if (dotx*dotx + doty*doty <= 1) {
-                // Punkt liegt innerhalb des Kreises
-                innerhalb++;
-            } else {
-                // Punkt liegt außerhalb des Kreises
-            }
-
-            tropfenzahl--;
+        for (double i = 0; i != 10000; i++) {
+            double k = i + 1;
+            pi = (Math.pow((-1), k))/(2*k+1);
         }
 
-        pi = 4*(double)innerhalb/gesamt;
-        System.out.println(pi);
-
+        System.out.println("Die Kreiszahl Pi: " + pi/4);
 
     }
 
--- src/dhbw/daniel/main/TextUmkehren.java
+++ src/dhbw/daniel/main/TextUmkehren.java
@@ -1,22 +1,58 @@
 package dhbw.daniel.main;
 
+/**
+ *  Die Zeichen eines String werden vertauscht
+ *
+ * @author danieljuric
+ * @version 1.0
+ *
+ *      23.10.2019
+ */
+
 public class TextUmkehren {
+    /**
+     * Main Methode
+     * @param args
+     * @deprecated Wird nicht benutzt
+     */
     public static void textUmkehren(String[] args) {
-        TextUmkehren textUmkehren = new TextUmkehren();
+        TextUmkehren textUmkehren = new TextUmkehren(); // Wird nicht benutzt
     }
 
+    /**
+     * Eigentliche Methode zum Text umkehren
+     *
+     * Hier wird ein Input mit einem Text gegeben und ein Result, welches am Anfang leer ist.
+     * Die for-Schleife macht nichts anderes als den Text von hinten nach Vorne durchzugehen
+     */
     public static void changeText() {
         String result = "";
         String input = "Hallo, das ist ein Test";
 
-        for (int pos = input.length(); pos > 0; pos--) {
-            String inputTeil = String.valueOf(input.charAt(pos-1));
+        for (int pos = input.length(); pos > 0; pos--) { // für Position = Länge des Textes; Solange Position größer 0; verrigere Position um 1
 
+            /**
+             * Der String inputTeil enthält immer genau 1 Zeichen, welches durch die Position aus der for-Schleife
+             * festgelegt ist.
+             *
+             * Demnach wird hier bei der Letzten Stelle angefangen und dann zwischen gespeichert (später in Result), die Position
+             * um eins Verringert und wiederholt
+             *
+             * ACHTUNG: Die erste Stelle eines Strings hat die Position 0, aber die Länge 1. Demnach wollen wir
+             *          Länge - 1 um die Position zu erhalten
+             * String.valueOf() ändert input.charAt() vom Typ Character in Typ String, da man in der Variable Character
+             * nur 1 Zeichen speichern kann
+             */
+            String inputTeil = String.valueOf(input.charAt(pos-1));  // Parsing Char to String
+
+            /**
+             * Füge nach jedem Durchlauf den Letzten Buchstaben nach vorn.
+             */
             result = result + inputTeil;
 
 
         }
-
+        // Ausgabe des Result
         System.out.println(result);
 
     }
